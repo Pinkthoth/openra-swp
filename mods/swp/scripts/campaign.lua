e6a2b8cc28cd6233f8a6502753059e460ad4182d
Pinkthoth <peurii@gmail.com>
2025-08-10 19:27:32 +0300

    0.045

---
+++ mods/swp/scripts/campaign.lua
@@ -0,0 +1,68 @@
+--[[
+   Copyright (c) The OpenRA Developers and Contributors
+   This file is part of OpenRA, which is free software. It is made
+   available to you under the terms of the GNU General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version. For more
+   information, see COPYING.
+]]
+
+Difficulty = Map.LobbyOptionOrDefault("difficulty", "normal")
+
+InitObjectives = function(player)
+	Trigger.OnObjectiveCompleted(player, function(p, id)
+		Media.DisplayMessage(p.GetObjectiveDescription(id), UserInterface.GetFluentMessage("objective-completed"))
+	end)
+	Trigger.OnObjectiveFailed(player, function(p, id)
+		Media.DisplayMessage(p.GetObjectiveDescription(id), UserInterface.GetFluentMessage("objective-failed"))
+	end)
+
+	Trigger.OnPlayerLost(player, function()
+		Trigger.AfterDelay(DateTime.Seconds(1), function()
+			Media.PlaySpeechNotification(player, "MissionFailed")
+		end)
+	end)
+
+	Trigger.OnPlayerWon(player, function()
+		Trigger.AfterDelay(DateTime.Seconds(1), function()
+			Media.PlaySpeechNotification(player, "MissionAccomplished")
+		end)
+	end)
+end
+
+AttackAircraftTargets = { }
+InitializeAttackAircraft = function(aircraft, enemyPlayer)
+	Trigger.OnIdle(aircraft, function()
+		local actorId = tostring(aircraft)
+		local target = AttackAircraftTargets[actorId]
+
+		if not target or not target.IsInWorld then
+			target = ChooseRandomTarget(aircraft, enemyPlayer)
+		end
+
+		if target then
+			AttackAircraftTargets[actorId] = target
+			aircraft.Attack(target)
+		else
+			AttackAircraftTargets[actorId] = nil
+			aircraft.ReturnToBase()
+		end
+	end)
+end
+
+ChooseRandomTarget = function(unit, enemyPlayer)
+	local target = nil
+	local enemies = Utils.Where(enemyPlayer.GetActors(), function(self)
+		return self.HasProperty("Health") and unit.CanTarget(self) and not Utils.Any({ "sbag", "fenc", "brik", "cycl", "barb" }, function(type) return self.Type == type end)
+	end)
+	if #enemies > 0 then
+		target = Utils.Random(enemies)
+	end
+	return target
+end
+
+OnAnyDamaged = function(actors, func)
+	Utils.Do(actors, function(actor)
+		Trigger.OnDamaged(actor, func)
+	end)
+end
